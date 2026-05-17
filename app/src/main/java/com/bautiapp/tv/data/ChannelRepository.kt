7b6af2762d43d9ed0b09bfc2681d24d496091d20
DupeMaxSab77 <hokah261@gmail.com>
2026-05-17 17:36:56 -0300

    Remove OkHttp dependency

--- app/src/main/java/com/bautiapp/tv/data/ChannelRepository.kt
+++ app/src/main/java/com/bautiapp/tv/data/ChannelRepository.kt
@@ -1,11 +1,8 @@
 package com.bautiapp.tv.data
 
 import com.google.gson.Gson
-import com.squareup.okhttp3.OkHttpClient
-import com.squareup.okhttp3.Request
 
 object ChannelRepository {
-    private val client = OkHttpClient()
     private val gson = Gson()
     
     suspend fun getChannels(): List<Channel> {
